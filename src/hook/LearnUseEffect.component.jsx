d76b2d6d7b84d0610f8d6126c2a95b882e0419a2
buiduchieu <duchieubui25012002@gmail.com>
2022-03-19 09:41:09 +0700

    add promise

--- src/hook/LearnUseEffect.component.jsx
+++ src/hook/LearnUseEffect.component.jsx
@@ -152,8 +152,8 @@ function LearnUseEffect() {
                 Toggle
             </button>
             {/* {toggle && <Content />} */}
-            {/* {toggle && <Avatar />} */}
-            {toggle && <Comment />}
+            {toggle && <Avatar />}
+            {/* {toggle && <Comment />} */}
         </div>
     )
     
--- src/reviewjs/review1.js
+++ src/reviewjs/review1.js
@@ -1,9 +1,33 @@
 /*
     - các khái niệm về promise
+
+    fetch API
+
+    setTimeout
+    setInterval
+
+
  */ 
 
 
 /*  This
     - Trong hầu hết mọi trường hợp khi sử dụng this thì từ khoá this sẽ trỏ về đối tượng mà đang thuộc về
     (this sẽ trỏ tới chính object gọi hàm đÓ)
+*/
+
+/*
+    setTimeout() : lệnh sẽ được thực thi 1 lần sau khoảng thời gian đã cho trước
+    vd:
+        myTimeout = setTimeout(function, milliseconds);
+    Để dừng thực thi setTimeout thì sử dụng clearTimeout()
+    vd:
+        clearTimeout(myTimeout);
+    setInterval() : lệnh sẽ được thực thi lặp lại sau khoảng thời gian cho trước
+    vd:
+        myInterval = setInterval(function, milliseconds);
+    Để dừng thực thi setInterval thì sử dụng clearInterval()
+    vd:
+        clearInterval(myInterval);
+
+
 */
